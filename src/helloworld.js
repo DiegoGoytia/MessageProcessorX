efd7ac5c12eaa47d523fdfd86d7fb7dfc7868c1d
A01376119 <a01376119@itesm.mx>
2019-08-15 11:06:19 -0500

    Added integration for posting requester information to Slack channel

--- src/helloworld.js
+++ src/helloworld.js
@@ -13,16 +13,21 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/user_creation_approval', function (req, res) {
+app.post('/user_creation_approval', function (req, res) {
+
+  var user_id = req.body.requester;
+
+  console.log(user_id);
+
   axios.post('https://hooks.slack.com/services/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', {
-    "text" : "Hello!!"
+    "text" : "A new user approval is waiting for response. (test)"
   })
   .then(function (response) {
-    console.log(response);
+    //console.log(response);
     res.send("Successfully connected to Slack API. Sending request.\n")
   })
   .catch(function (error) {
-    console.log(error);
+    //console.log(error);
     res.send("Something went wrong when attempting connectivity to Slack API.\n")
   })
 });
